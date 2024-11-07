15a4cf562aa0edd003e34995ec33ef7c6d20cd00
Karim Moataz <karimmoataz42@gmail.com>
2024-11-08 00:29:30 +0200

    commit

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -3,38 +3,42 @@ import { ragChat } from '@/lib/rag-chat'
 import { redis } from '@/lib/redis'
 import { cookies } from 'next/headers'
 import React from 'react'
-interface PageProps{
-    params:{
-        url: string | string[] | undefined
-    }
-}
-function reconstructedUrl ({url}:{url:string[]}){
-    const decodedComponents = url.map((component) => decodeURIComponent(component))
-    return decodedComponents.join("/")
+
+interface PageProps {
+  params: {
+    url: string[] | string | undefined
+  }
 }
 
-const page = async ({params}:PageProps) => {
-    const sessionCookie= (await cookies()).get("sessionId")?.value
-    const reconstructedUrlValue  = reconstructedUrl({url: params.url as string[]})
+function reconstructedUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) => decodeURIComponent(component))
+  return decodedComponents.join("/")
+}
 
-    const sessionId = (reconstructedUrl + "--" + sessionCookie).replace(/\//g,"")
+const Page = async ({ params }: PageProps) => {
+  const urlArray = Array.isArray(params.url) ? params.url : params.url ? [params.url] : [];
 
-    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrlValue )
+  const sessionCookie = (await cookies()).get("sessionId")?.value
+  const reconstructedUrlValue = reconstructedUrl({ url: urlArray })
 
-    const initialMessages = await ragChat.history.getMessages({amount:10, sessionId})
-    
+  const sessionId = (reconstructedUrlValue + "--" + sessionCookie).replace(/\//g, "")
 
-    if(!isAlreadyIndexed){
-        await ragChat.context.add({
-            type: "html",
-            source: reconstructedUrlValue ,
-            config: {chunkOverlap: 50, chunkSize:200},
-        })
-        await redis.sadd("indexed-urls", reconstructedUrlValue )
-    }
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrlValue)
+
+  const initialMessages = await ragChat.history.getMessages({ amount: 10, sessionId })
+
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: reconstructedUrlValue,
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    })
+    await redis.sadd("indexed-urls", reconstructedUrlValue)
+  }
+
   return (
-    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages}/>
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
   )
 }
 
-export default page
+export default Page
